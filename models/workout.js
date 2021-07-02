2fae77fafa48b86959f951a14621eeba640dd1b1
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-03 00:09:39 +1000

    models and basic server.js file

--- models/workout.js
+++ models/workout.js
@@ -1,14 +1,44 @@
-// To write schema code here
 // call for required package (i.e., mongoose)
+const mongoose = require("mongoose");
+
 // create const Schema
+const Schema = mongoose.Schema;
+
 // create const workoutSchema and add content based on seed.js file
-    //day - include type as date and default as current date and time
-    // exercise - include the following:
-        // type as string
-        // name as string
-        // duration as number
-        // weight as number
-        // reps as number
-        // sets as number
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date, //day - include type as date and default as current date and time
+            default: Date.now
+        },
+        exercises: [ // exercise - include the following:
+        {
+            type: { 
+                type: String, // type as string
+                trim: true,
+            },
+            name: { 
+                type: String, // name as string
+                trim: true,
+            },
+            duration: {
+                type: Number, // duration as number
+            },
+            weight: {
+                type: Number, // weight as number
+            },
+            reps: {
+                type: Number, // reps as number
+            },
+            sets: {
+                type: Number, // sets as number
+            },
+        }
+        ],          
+    });
+
 // create mongoose model
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
 // export module
+module.exports = Workout;
