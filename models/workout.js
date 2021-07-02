9f1f287299406c0045c571fdc2ac12140627399c
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-02 23:39:36 +1000

    psuedo codes for models

---
+++ models/workout.js
@@ -0,0 +1,14 @@
+// To write schema code here
+// call for required package (i.e., mongoose)
+// create const Schema
+// create const workoutSchema and add content based on seed.js file
+    //day - include type as date and default as current date and time
+    // exercise - include the following:
+        // type as string
+        // name as string
+        // duration as number
+        // weight as number
+        // reps as number
+        // sets as number
+// create mongoose model
+// export module
