81d5dcdac36b96ec21f7707d1e7736c3f5cd75ef
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-05 22:44:51 +1000

    deploying

--- routes/api.js
+++ routes/api.js
@@ -13,4 +13,56 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+
+// Updating the exisitng entry by id
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    db.Workout.findByIdAndUpdate(
+      {_id: req.params.id}, 
+      { $push: {exercises: req.body} },
+      { upsert: true }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  //route to find all workouts
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration"},
+        },
+      },
+    ])
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+  //route to find details for last 7 workouts
+  router.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration"},
+        },
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(7)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 module.exports = router;
